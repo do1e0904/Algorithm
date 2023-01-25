fb333be2b937f2967888f33bd192407893289dd6
zcx1477 <74893989+zcx1477@users.noreply.github.com>
2023-01-26 00:57:25 +0900

    Create 2805.py

---
+++ BOJ/2805.py
@@ -0,0 +1,22 @@
+# 이분탐색
+import sys
+n,m=map(int,input().split())
+a=list(map(int, sys.stdin.readline().split()))
+a.sort(reverse=True)
+
+def height(start,end):
+    tmp=0
+    h=(start+end)//2
+    for i in range(n):
+        if a[i] > h:
+            tmp+=(a[i]-h)
+    if h == start or h == end:
+        print(h)
+    elif tmp < m:
+        height(start, h)
+    elif tmp > m:
+        height(h, end)
+    elif tmp == m:
+        print(h)
+        
+height(0,a[0])
