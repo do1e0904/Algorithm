2bd15786808525c122e752b2a5af819f176e8b6b
do1e <74893989+zcx1477@users.noreply.github.com>
2024-07-11 03:50:39 +0900

    [Bronze I] Title: 숫자 카드, Time: 120 ms, Memory: 111240 KB -BaekjoonHub

---
+++ 백준/Bronze/21394. 숫자 카드/숫자 카드.py
@@ -0,0 +1,20 @@
+T = int(input())
+for _ in range(T):
+    answer = []
+    x = list(map(int, input().split()))
+    num = []
+    for i in range(9):
+        for _ in range(x[i]):
+            if i == 5:
+                num.append(9)
+            else:
+                num.append(i+1)
+    num.sort()
+    for i in range(len(num)-1, -1, -2):
+        answer.append(num[i])
+    for i in range(len(num)%2, len(num), 2):
+        answer.append(num[i])
+
+    for i in range(len(answer)):
+        print(answer[i], end=' ')
+    print()
