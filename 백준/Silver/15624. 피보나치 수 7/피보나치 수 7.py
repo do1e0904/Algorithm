23fd05e1861e68dac5bd3f65abbb935ab9b60232
do1e <74893989+zcx1477@users.noreply.github.com>
2024-09-06 16:23:03 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 132 ms, Memory: 184624 KB, Score: 201 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
@@ -1,6 +1,10 @@
+n = int(input())
 dp = []
 dp.append(0)
 dp.append(1)
-for i in range(int(input())-1):
+for i in range(n-1):
     dp.append((dp[i]+dp[i+1])%1000000007)
-print(dp[len(dp)-1])
+if n:
+    print(dp[len(dp)-1])
+else:
+    print("0")
