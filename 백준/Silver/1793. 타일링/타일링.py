b2b7738aaa56e8a928cf020a2229ea55d5df8624
do1e <74893989+zcx1477@users.noreply.github.com>
2024-09-07 12:36:30 +0900

    [Silver II] Title: 타일링, Time: 104 ms, Memory: 110404 KB -BaekjoonHub

---
+++ 백준/Silver/1793. 타일링/타일링.py
@@ -0,0 +1,12 @@
+while True:
+    try:
+        n = int(input())
+        dp = [0] * 251
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2] * 2
+        print(dp[n])
+        
+    except EOFError:
+        break
