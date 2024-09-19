0e52f31d2ff5ae08bec53c4d1bdf3f8728b61374
do1e <74893989+zcx1477@users.noreply.github.com>
2024-09-19 12:49:11 +0900

    [Gold IV] Title: 두 배, Time: 180 ms, Memory: 157264 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31963. 두 배/두 배.py
+++ 백준/Gold/31963. 두 배/두 배.py
@@ -1,8 +1,17 @@
+import math
+
 N = int(input())
 a = list(map(int, input().split()))
+for i in range(N):
+    a[i] = math.log2(a[i])
+    
 cnt = 0
 for i in range(1, N):
-    while a[i] < a[i-1]:
-        a[i] *= 2
-        cnt += 1
+    j = 0
+    if a[i-1] > a[i]:
+        j = int(a[i-1]-a[i])
+    if a[i-1] > a[i]+j:
+        j += 1
+    a[i] += j
+    cnt += j
 print(cnt)
